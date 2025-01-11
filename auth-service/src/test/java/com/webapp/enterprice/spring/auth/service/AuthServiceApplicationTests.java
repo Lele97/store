845bab9c6881cc1d3a79410e59463c381f97c9fa
Lele97 <gabriele.grandienetti@ehotmail.com.>
2025-01-11 13:26:04 +0100

    commit

---
+++ auth-service/src/test/java/com/webapp/enterprice/spring/auth/service/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webapp.enterprice.spring.auth.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
