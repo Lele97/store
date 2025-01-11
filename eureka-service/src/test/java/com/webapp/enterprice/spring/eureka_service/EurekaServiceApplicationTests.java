00623de1588367a357d2ca6f0c67d1dc417be333
Lele97 <gabriele.grandienetti@ehotmail.com.>
2025-01-11 11:42:00 +0100

    commit

---
+++ eureka-service/src/test/java/com/webapp/enterprice/spring/eureka_service/EurekaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webapp.enterprice.spring.eureka_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
