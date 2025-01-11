69c91114e32b215551ac565246e84c924e1a733a
Lele97 <gabriele.grandienetti@ehotmail.com.>
2025-01-11 21:52:25 +0100

    commit

--- auth-service/src/main/java/com/webapp/enterprice/spring/auth/service/controller/AuthController.java
+++ auth-service/src/main/java/com/webapp/enterprice/spring/auth/service/controller/AuthController.java
@@ -7,10 +7,11 @@ import com.webapp.enterprice.spring.auth.service.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -19,7 +20,7 @@ import org.springframework.web.bind.annotation.*;
 public class AuthController {
 
     @Autowired
-    private  UserService service;
+    private UserService service;
 
     @Autowired
     private JwtService jwtService;
@@ -27,22 +28,28 @@ public class AuthController {
     @Autowired
     private AuthenticationManager authenticationManager;
 
-
     @PostMapping(path = "/register")
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void register(@RequestBody UserRequest userRequest) {
-        service.registration(userRequest);
+    public ResponseEntity<String> register(@RequestBody UserRequest userRequest) {
+        try {
+            service.registration(userRequest);
+            return new ResponseEntity<>("Registration successful", HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
     @PostMapping("/login")
-    public String authenticateAndGetToken(@RequestBody AuthRequest authRequest) {
-        Authentication authentication = authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(authRequest.getEmail(), authRequest.getPassword())
-        );
-        if (authentication.isAuthenticated()) {
-            return jwtService.generateToken(authRequest.getEmail());
-        } else {
-            throw new UsernameNotFoundException("Invalid user request!");
+    public ResponseEntity<String> authenticateAndGetToken(@RequestBody AuthRequest authRequest) {
+        try {
+            Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(authRequest.getEmail(), authRequest.getPassword()));
+            if (authentication.isAuthenticated()) {
+                String token = jwtService.generateToken(authRequest.getEmail());
+                return new ResponseEntity<>(token, HttpStatus.OK);
+            } else {
+                return new ResponseEntity<>("Invalid user request!", HttpStatus.UNAUTHORIZED);
+            }
+        } catch (AuthenticationException e) {
+            return new ResponseEntity<>("Authentication failed: " + e.getMessage(), HttpStatus.UNAUTHORIZED);
         }
     }
 }
--- auth-service/src/main/java/com/webapp/enterprice/spring/auth/service/service/UserService.java
+++ auth-service/src/main/java/com/webapp/enterprice/spring/auth/service/service/UserService.java
@@ -4,38 +4,34 @@ import com.webapp.enterprice.spring.auth.service.entity.User;
 import com.webapp.enterprice.spring.auth.service.entity.UserRequest;
 import com.webapp.enterprice.spring.auth.service.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.log4j.Log4j;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-
-
-
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
 
     @Autowired
-    private  UserRepository repository;
+    private UserRepository repository;
+
     @Autowired
     private PasswordEncoder bCryptPasswordEncoder;
 
-    public void registration(UserRequest userRequest) {
+    public void registration(UserRequest userRequest) throws Exception {
+        if (repository.findByEmail(userRequest.getEmail()).isPresent()) {
+            throw new Exception(userRequest.getUsername() + " already exists");
+        }
         try {
             User user = new User();
             user.setUsername(userRequest.getUsername());
             user.setPassword(bCryptPasswordEncoder.encode(userRequest.getPassword()));
             user.setEmail(userRequest.getEmail());
             user.setRoles(userRequest.getRoles());
-            if (repository.findByEmail(user.getEmail()).isPresent()) {
-                throw new Exception(userRequest.getUsername() + " already exists");
-            }
             repository.save(user);
-            //log.log(Level.INFO, "Registration successful");
-
         } catch (Exception e) {
-            //log.log(Level.SEVERE, e.getMessage());
+            throw new Exception("Registration failed: " + e.getMessage(), e);
         }
     }
 }
