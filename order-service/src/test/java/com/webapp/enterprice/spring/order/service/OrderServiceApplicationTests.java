4ce508fe64f96ce8ffb6686036f13b18465011e4
Lele97 <gabriele.grandienetti@ehotmail.com.>
2025-01-11 23:34:44 +0100

    commit

---
+++ order-service/src/test/java/com/webapp/enterprice/spring/order/service/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webapp.enterprice.spring.order.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ product-service/src/test/java/com/webapp/enterprice/spring/product/service/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webapp.enterprice.spring.product.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
