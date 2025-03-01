0ee1ca32120b1435346eb47d2b1718a3db49df09
Lele97 <gabriele.grandienetti@ehotmail.com.>
2025-03-01 17:00:27 +0100

    update junit test classes

--- auth-service/src/test/java/com/webapp/enterprice/spring/auth/service/AuthServiceApplicationTests.java
+++ auth-service/src/test/java/com/webapp/enterprice/spring/auth/service/AuthServiceApplicationTests.java
@@ -1,10 +1,38 @@
 package com.webapp.enterprice.spring.auth.service;
 
+import com.webapp.enterprice.spring.auth.service.controller.AuthController;
+import com.webapp.enterprice.spring.auth.service.service.UserService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.springframework.test.util.AssertionErrors.assertNotNull;
 
 @SpringBootTest
 class AuthServiceApplicationTests {
+
+    @Autowired
+    private UserService service;
+
+    @Autowired
+    private AuthController controller;
+
+    @Value("${db.password}")
+    private String dbPassword;
+
+    @Value("${db.username}")
+    private String dbusername;
+
     @Test
-    void contextLoads() {}
+    void contextLoads() {
+
+        // Check that beans are properly loaded
+        assertNotNull("UserService bean should not be null", service);
+        assertNotNull("AuthController bean should not be null", controller);
+
+        // Verify configuration properties
+        assertNotNull("DB Password should not be null", dbPassword);
+        assertNotNull("DB Username should not be null", dbusername);
+    }
 }
--- product-service/src/test/java/com/webapp/enterprice/spring/product/service/ProductServiceApplicationTests.java
+++ product-service/src/test/java/com/webapp/enterprice/spring/product/service/ProductServiceApplicationTests.java
@@ -1,13 +1,38 @@
 package com.webapp.enterprice.spring.product.service;
 
+import com.webapp.enterprice.spring.product.service.controller.ProductController;
+import com.webapp.enterprice.spring.product.service.service.ProductService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.springframework.test.util.AssertionErrors.assertNotNull;
 
 @SpringBootTest
 class ProductServiceApplicationTests {
 
+	@Autowired
+	private ProductService service;
+
+	@Autowired
+	private ProductController controller;
+
+	@Value("${db.password}")
+	private String dbPassword;
+
+	@Value("${db.username}")
+	private String dbusername;
+
 	@Test
 	void contextLoads() {
-	}
 
+		// Check that beans are properly loaded
+		assertNotNull("UserService bean should not be null", service);
+		assertNotNull("AuthController bean should not be null", controller);
+
+		// Verify configuration properties
+		assertNotNull("DB Password should not be null", dbPassword);
+		assertNotNull("DB Username should not be null", dbusername);
+	}
 }
